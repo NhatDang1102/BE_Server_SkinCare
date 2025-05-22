1611eccb31cc8a8d9a0f731ff05b5a7a5c1c84b0
NhatDang1102 <nezukosama247@gmail.com>
2025-05-22 22:40:28 +0700

    refactor : injection cho mainapp

---
+++ Main/AppInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Helpers;
+using Repository.Models;
+
+namespace MainApp
+{
+    public static class AppInjection
+    {
+        public static IServiceCollection AddMainAppServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
+            services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+
+            services.AddSingleton<MailSender>();
+            services.AddDbContext<SkinCareAppContext>();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+                });
+
+            return services;
+        }
+    }
+}
