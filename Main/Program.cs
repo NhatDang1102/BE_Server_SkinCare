2e1e0b787c812ce93a3a39f697e6d02adca73644
NhatDang1102 <nezukosama247@gmail.com>
2025-05-22 22:40:28 +0700

    refactor : injection cho mainapp

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using MainApp;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -22,27 +23,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings")); 
-builder.Services.AddSingleton<MailSender>();
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+
+builder.Services.AddMainAppServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 builder.Services.AddServiceServices();
-builder.Services.AddDbContext<SkinCareAppContext>();
 
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
