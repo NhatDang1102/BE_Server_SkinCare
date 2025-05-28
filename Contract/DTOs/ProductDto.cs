a3ddc48384a181bba1c659f9e1e1fd11e68bb233
NhatDang1102 <nezukosama247@gmail.com>
2025-05-28 19:16:14 +0700

    feat: Setup product + cate dtos

---
+++ Contract/DTOs/ProductDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.DTOs
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string ProductLink { get; set; }
+        public string ImageLink { get; set; }
+        public List<CategoryDto> Categories { get; set; }
+    }
+
+    public class CreateUpdateProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string ProductLink { get; set; }
+        public string ImageLink { get; set; }
+        public List<Guid> CategoryIds { get; set; }
+    }
+
+}
