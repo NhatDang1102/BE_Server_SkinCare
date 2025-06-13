263da6c74b179814ac9c464726cbd408bfc8cabc
NhatDang1102 <nezukosama247@gmail.com>
2025-06-13 22:26:50 +0700

    feat: Repo cho Routine (CRUD các bảng Routine)

---
+++ Contract/DTOs/RoutineDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Contract.DTOs
+{
+    public class RoutineRequestDto
+    {
+        public IFormFile Image { get; set; }
+    }
+
+    public class RoutineResultDto
+    {
+        public List<ProductDto> Morning { get; set; }
+        public List<ProductDto> Noon { get; set; }
+        public List<ProductDto> Night { get; set; }
+        public string Advice { get; set; }
+    }
+
+    public class RoutineResultFullDto
+    {
+        public List<ProductDto> Morning { get; set; }
+        public List<ProductDto> Noon { get; set; }
+        public List<ProductDto> Night { get; set; }
+        public string Advice { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+
+}
