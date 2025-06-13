4facee59d54bd291806fbd18379f29443ca427ad
NhatDang1102 <nezukosama247@gmail.com>
2025-06-13 22:26:50 +0700

    feat: Repo cho Routine (CRUD các bảng Routine)

---
+++ Repository/Interfaces/IDailyRoutineRepository.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Models;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IDailyRoutineRepository
+    {
+        Task AddAsync(DailyRoutine routine);
+        Task AddRoutineProductAsync(DailyRoutineProduct mapping);
+        Task<DailyRoutine> GetByUserIdAsync(Guid userId);
+        Task DeleteRoutineProductsByRoutineIdAsync(Guid routineId);
+        Task DeleteAsync(DailyRoutine routine);
+    }
+}
---
+++ Repository/Repositories/DailyRoutineRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+using Repository.Models;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class DailyRoutineRepository : IDailyRoutineRepository
+    {
+        private readonly SkinCareAppContext _ctx;
+        public DailyRoutineRepository(SkinCareAppContext ctx) { _ctx = ctx; }
+
+        public async Task AddAsync(DailyRoutine routine)
+        {
+            _ctx.DailyRoutines.Add(routine);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task AddRoutineProductAsync(DailyRoutineProduct mapping)
+        {
+            _ctx.DailyRoutineProducts.Add(mapping);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<DailyRoutine> GetByUserIdAsync(Guid userId)
+        {
+            return await _ctx.DailyRoutines.FirstOrDefaultAsync(r => r.UserId == userId);
+        }
+
+        public async Task DeleteRoutineProductsByRoutineIdAsync(Guid routineId)
+        {
+            var mappings = _ctx.DailyRoutineProducts.Where(m => m.DailyRoutineId == routineId);
+            _ctx.DailyRoutineProducts.RemoveRange(mappings);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(DailyRoutine routine)
+        {
+            _ctx.DailyRoutines.Remove(routine);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
