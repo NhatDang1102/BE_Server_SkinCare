34f2a24a6c777aaca2d55455ecc34edc62d72768
NhatDang1102 <nezukosama247@gmail.com>
2025-07-01 15:52:13 +0700

    fix: Webhook tự động

--- Main/Controllers/VipPaymentController.cs
+++ Main/Controllers/VipPaymentController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Net.payOS.Types;
+using Newtonsoft.Json;
+using Service.Interfaces;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Contract.DTOs;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Service.Interfaces;
 
 namespace MainApp.Controllers
 {
@@ -23,22 +23,25 @@ namespace MainApp.Controllers
         [Authorize]
         public async Task<IActionResult> CreateLink()
         {
-            //lay userid
             var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier)
                 ?? User.FindFirstValue("id")
                 ?? User.FindFirstValue("sub");
+
             if (!Guid.TryParse(userIdStr, out var userId))
-                return Unauthorized("Không tim thay user");
+                return Unauthorized("Không tìm thấy user");
 
             var url = await _vipPaymentService.CreateVipPaymentLinkAsync(userId);
             return Ok(new { checkoutUrl = url });
         }
 
-        [HttpPost("webhook")]
-        public async Task<IActionResult> PayOSWebhook([FromBody] PayOSWebhook data)
+        [HttpPost("receive-hook")]
+        public async Task<IActionResult> PayOSWebhook([FromBody] WebhookType webhookBody)
         {
-            await _vipPaymentService.HandlePayOSWebhookAsync(data);
-            return Ok(new { success = true });
+            Console.WriteLine("Webhook received: " + JsonConvert.SerializeObject(webhookBody));
+            var handled = await _vipPaymentService.HandlePayOSWebhookAsync(webhookBody);
+            Console.WriteLine("Webhook handled: " + handled);
+            return Ok(new { success = handled });
         }
+
     }
 }
--- Service/Interfaces/IVipPaymentService.cs
+++ Service/Interfaces/IVipPaymentService.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Contract.DTOs;
+using Net.payOS.Types;
 
 namespace Service.Interfaces
 {
     public interface IVipPaymentService
     {
         Task<string> CreateVipPaymentLinkAsync(Guid userId);
-        Task<bool> HandlePayOSWebhookAsync(PayOSWebhook data); 
-    }
+        Task<bool> HandlePayOSWebhookAsync(WebhookType webhookBody);
+            }
 }
